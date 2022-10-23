d3374d3f63dd41b4e0dbbe9f9bbf746a712934e8
serhat0ner <serhatoner@protonmail.com>
2022-10-23 15:04:39 +0300

    created verifyTokenAdmin middleware

--- routes/user.js
+++ routes/user.js
@@ -8,8 +8,9 @@ dotenv.config();
 
 
 //INFO: an update function, therefore put.
+//INFO: verifyTokenAndAuthorization: a middleware function, next: async(req,res)
 router.put("/:id", verifyTokenAndAuthorization, async (req,res)=> {
-    if(req.body.password){ //INFO: we should check password before update.
+    if(req.body.password){ //TIP: new password given, we should encrypt it before sending it to db.
         req.body.password = cryptojs.AES
         .encrypt(req.body.password, process.env.JWT_SECRET_KEY)
         .toString();
@@ -20,7 +21,7 @@ router.put("/:id", verifyTokenAndAuthorization, async (req,res)=> {
             $set:req.body
         }, {new:true})//INFO:new true makes this function to return new updated user.
 
-        const {password, ...others} = updatedUser;
+        const {password, ...others} = updatedUser._doc;//TIP: always get doc from updateduser or created user.
 
         res.status(200).json(others);
     } catch(err){
@@ -29,6 +30,15 @@ router.put("/:id", verifyTokenAndAuthorization, async (req,res)=> {
 })
 
 
+
+router.delete("/:id", verifyTokenAndAuthorization, async (req,res)=> {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200.).json("User has been deleted");
+    } catch (error) {
+        res.status(500).json(err);
+    }
+})
 
 
 module.exports = router;
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -10,7 +10,7 @@ const verifyToken = (req,res, next) => {
     if(authHeader) { //INFO: err,user => if everything is okey, verifyTOken will return us a DATA, you can name it whatever you like we called it user here.
         jwt.verify(token, process.env.JWT_SECRET_KEY, (err,user)=> {
             if(err) res.status(403).json("Token is not valid");
-            req.user = user;
+            req.user = user; //INFO: WE PASS VALUE FROM MIDDLEWARE TO REQ BODY TO PASS DATA TO OTHER FUNCTIONS!
             next();//INFO: leave this function and go router.
         });
     } else {
@@ -21,13 +21,25 @@ const verifyToken = (req,res, next) => {
 
 
 const verifyTokenAndAuthorization = (req,res,next) => {
-    verifyToken(req,res, ()=> {
+    verifyToken(req,res, ()=>   {
         if(req.user.id === req.params.id || req.user.isAdmin){
             next();
         } else {
             res.status(403).json("You are not authorized")
         }
     })
+}
+
+//TIP: some stuff just admins can do such as adding product to website..
+
+const verifyTokenAndAdmin =(req,res, next)=> {
+    verifyToken(req,res,()=> {
+        if(req.user.isAdmin){
+            next();
+        } else {
+            res.status(403).json("You are not authorized");
+        }
+    }); 
 }
 
 
